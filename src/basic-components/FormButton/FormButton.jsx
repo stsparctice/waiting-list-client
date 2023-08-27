29dc3e9dd822a09add1c6a071c4f75a5fd87b7e1
stsparctice <131611269+stsparctice@users.noreply.github.com>
2023-08-27 03:05:32 +0300

    add form style

---
+++ src/basic-components/FormButton/FormButton.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    btn: {
+        padding: [8, 20],
+        margin:10,
+        backgroundColor: 'black',
+        color: 'white',
+        letterSpacing: 1.03,
+        borderRadius: 5,
+
+        transitionDuration: '0.3s',
+        transitionProperty: 'all',
+        transitionTimingFunction: 'linear',
+        '&:hover': {
+            cursor: 'pointer',
+            borderRadius: 20,
+
+        }
+    }
+})
+
+const FormButton = ({ text, func }) => {
+    const css = useStyles()
+    return <>
+        <input type="button" className={css.btn} value={text} onClick={func} />
+    </>
+}
+export default FormButton;
